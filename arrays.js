86f156125ae630c69f904aef565f3f498e3ac4cb
Bethany Oestrich <bethany@oestrich.org>
2018-04-07 00:53:57 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars =[
+  'snickers', 'hundred grand', 'kitkat', 'skittles'];
+  
+function addElementToBeginningOfArray(array,newElement) {
+   return [newElement, ...array]
+ }
+ 
+ function destructivelyAddElementToBeginningOfArray(array,newElement) {
+   array.unshift(newElement)
+   return array
+ }
+ 
+ function addElementToEndOfArray (array,newElement){
+   return [...array, newElement]
+ }
+ 
+ function destructivelyAddElementToEndOfArray(array,newElement){
+   array.push(newElement)
+   return array}
